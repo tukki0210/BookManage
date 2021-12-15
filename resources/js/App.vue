820bafd0298ab5d96819580bc4d47e997b87bbfc
tukki <tukki0210@gmail.com>
2021-12-15 02:33:26 +0000

    init

---
+++ resources/js/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    {{ data.message }}
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import { reactive } from "vue";
+import HelloWorld from "./components/HelloWorld";
+
+import axios from "axios";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  setup() {
+    const data = reactive({
+      message: "Hello Vue!!!!!!!!",
+    });
+    const url = "http://127.0.0.1:8001/api/books";
+    const getAPI = async () => {
+      const result = await axios.get(url);
+      console.log(result);
+    };
+
+    getAPI();
+
+    return {
+      data,
+      getAPI,
+    };
+  },
+};
+</script>
